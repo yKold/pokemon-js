328c418e9085c7842c8a173400fa7c44882fb66d
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-12-27 13:36:04 -0300

    a

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+for (let i = 0; i < collisions.length; i+=70){
+    console.log(collisions.slice(i, 70 + i))
+}
 
 const image = new Image()
 image.src = './assets/map.png'
@@ -16,33 +17,27 @@ playerImage.src = './assets/playerDown.png'
 image.onload = () => {
     c.drawImage(image, -540,-570 )
 }
-playerImage.onload = () => {
-    c.drawImage(playerImage, 
-        canvas.width/2 - playerImage.width/2,
-        canvas.height/2 - playerImage.height/2
-        )
+
+
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+    
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
 
-// class Sprite {
-//     constructor({position, velocity, image}) {
-//         this.position = position
-//         this.image = image
-//     }
+const background = new Sprite({
+    position: {
+        x: -540,
+        y: -570
+    },
+    image: image
     
-//     draw() {
-//         c.drawImage(this.image, this.position.x, this.position.y)
-//     }
-// }
-
-// const background = new Sprite({
-//     position: {
-//         x: -785,
-//         y: -650
-//     },
-//     image: {
-//         image
-//     }
-// })
+})
 
 const keys = {
     w: {
@@ -59,24 +54,29 @@ const keys = {
     }
 }
 
-// function animate() {
-//     window.requestAnimationFrame(animate)
-//     background.draw()
-//     c.drawImage(playerImage, 0, 0, 0, 0)
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    c.drawImage(playerImage, 
+        canvas.width/2 - playerImage.width/2,
+        canvas.height/2 - playerImage.height/2
+        )
 
-//     if (keys.w.pressed && lastKey === 'w') {
-//         background.position.y += 3
-//     } else if (keys.s.pressed && lastKey === 'a') {
-//         background.position.y -= 3
-//     } else if (keys.a.pressed && lastKey === 's') {
-//         background.position.x += 3
-//     } else if (keys.d.pressed && lastKey === 'd') {
-//         background.position.x -= 3
-//     }
-// }
+    if (keys.w.pressed && lastKey === 'w') {
+        background.position.y += 3
+    } else if (keys.s.pressed && lastKey === 's') {
+        background.position.y -= 3
+    } else if (keys.a.pressed && lastKey === 'a') {
+        background.position.x += 3
+    } else if (keys.d.pressed && lastKey === 'd') {
+        background.position.x -= 3
+    }
+}
+animate()
 
-// let lastKey = ''
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
+    console.log(e)
     switch (e.key){
         case 'w':
             keys.w.pressed = true
@@ -97,19 +97,19 @@ window.addEventListener('keydown', (e) => {
     }
 })
 
-// window.addEventListener('up', (e) => {
-//     switch (e.key){
-//         case 'w':
-//             keys.w.pressed = false
-//             break
-//         case 'a':
-//             keys.a.pressed = false
-//             break
-//         case 's':
-//             keys.s.pressed = false
-//             break
-//         case 'd':
-//             keys.d.pressed = false
-//             break
-//     }
-// })
+window.addEventListener('keyup', (e) => {
+    switch (e.key){
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+})
